8ac319582329920f2c090eb5f380820f108830d3
Shahadat Hossain <sht.kcn@gmail.com>
2026-01-15 13:51:01 +0600

    Creating a new post_update file

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
-from .views import PostListView, PostDetailView, PostCreateView
-
+from .views import ( 
+    PostListView, 
+    PostDetailView, 
+    PostCreateView,
+    PostUpdateView,
+)
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'), # new
     path('post/new/', PostCreateView.as_view(), name='post_new'), # new
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'), # new
 ]
