d8f993635f69e2272f669fbe7ece1f160b5ccb57
Shahadat Hossain <sht.kcn@gmail.com>
2026-01-15 13:13:19 +0600

    Changing funciton-based view to class-based views

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, 'home.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'post_detail.html', {'post': post})
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'posts'
+
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'post_detail.html', {'post': post})
+
+class PostDetailView(ListView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
